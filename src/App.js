98c93a314c6b12582dc5c341d339b7a37dc2bb6f
yogesh <yogesh@blockgemini.com>
2018-12-31 17:08:35 +0400

    contact list

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Sidebar from './components/sidebar/sidebar';
 import Main from './components/main/main';
-
+import store from "./store";
+import _ from "lodash";
 
 class App extends Component {
   render() {
+    const { contacts } = store.getState();
+
       return (
         <div className="App">
-          <Sidebar />
+          <Sidebar  contacts={_.values(contacts)} />
           <Main />
         </div>
       );
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,8 +1,10 @@
 import React from "react";
+import User from "../user/user"; 
 import "./sidebar.css";
 
-const Sidebar = () => {
-  return <aside className="Sidebar">Sidebar</aside>;
+const Sidebar = ({ contacts }) => {
+  return <aside className="Sidebar"><p>Contacts</p>
+     {contacts.map(contact => <User user={contact} key={contact.user_id} />)}</aside>;
 };
 
 export default Sidebar;
